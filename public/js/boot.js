7dd82227a0d9a019a031fb3d60a6980e87073dca
bciv <bciv@Phoenix.local>
2015-11-02 19:04:54 -0500

    added contacts, status,  associated collections.

---
+++ public/js/boot.js
@@ -0,0 +1,22 @@
+require.config({
+  paths: {
+    jQuery: '/js/libs/jquery',
+    Bootstrap: '/js/libs/bootstrap',
+    Underscore: '/js/libs/underscore',
+    Backbone: '/js/libs/backbone',
+    models: 'models',
+    text: '/js/libs/text',
+    templates: '../templates',
+
+    SocialNetView: '/js/SocialNetView'
+  },
+
+  shim: {
+    'Backbone': ['Underscore', 'jQuery'],
+    'SocialNet': ['Backbone']
+  }
+});
+
+require(['SocialNet'], function(SocialNet) {
+  SocialNet.initialize();
+});
--- public/js/views/FooterView.js
+++
@@ -1,7 +0,0 @@
-define([ 'marionette', 'handlebars', 'text!templates/footer.html'],
-    function (Marionette, Handlebars, template) {
-        //ItemView provides some default rendering logic
-        return Marionette.ItemView.extend({
-            template:Handlebars.compile(template)
-        });
-    });
